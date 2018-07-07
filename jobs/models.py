abd048eeaeaaf79009422ea07bac57505b8a561f
Ian Fraser <icf0001@auburn.edu>
2018-07-06 23:14:19 -0500

    added SummerNote and buttons to cards

--- jobs/models.py
+++ jobs/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=200)
-    summary = models.CharField(max_length=200)
+    summary = models.TextField(max_length=2000)
 
     def __str__(self):
         return self.title
--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -21,7 +21,8 @@ from django.urls import include, path
 import jobs.views
 
 urlpatterns = [
+    path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-    # path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
